ffc62abd6b583dee214e6a5283427e63fb2b4504
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-06-25 15:28:34 -0500

    logro: Implementar y usar JpaRepository, una interfaz hija de Repository

--- src/main/java/com/bycompany/persistence/repository/AdressCrudRepository.java
+++ src/main/java/com/bycompany/persistence/repository/AdressCrudRepository.java
@@ -1,4 +1,8 @@
 package com.bycompany.persistence.repository;
 
-public interface AdressCrudRepository {
+import com.bycompany.persistence.entity.Adress;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdressCrudRepository extends JpaRepository<Adress, Long> {
+
 }
